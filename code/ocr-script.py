7419dae63eeda4b211ed6e71dd82200e27400ecf
Renato de Angelis <renatodeangelis12@gmail.com>
2024-11-19 15:18:07 -0600

    Some changes

--- code/ocr-script.py
+++ code/ocr-script.py
@@ -1,9 +1,6 @@
-from PIL import Image
+import os
+import pdfplumber
+import pandas as pd
 
-import pytesseract
+directory = "../data/cds"
 
-try:
-    text = pytesseract.image_to_string(Image.new('RGB', (100, 30), color='white'))
-    print("pytesseract is working! Detected text:", text)
-except pytesseract.TesseractNotFoundError:
-    print("Tesseract is not found. Ensure it's installed and in your PATH.")
