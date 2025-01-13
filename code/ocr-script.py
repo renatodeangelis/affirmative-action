0719ed9bd37b79a0fdf0ed46c7b90227e75bc14c
Renato de Angelis <renatodeangelis12@gmail.com>
2025-01-13 15:26:25 -0600

    typo

--- code/ocr-script.py
+++ code/ocr-script.py
@@ -2,11 +2,12 @@ import pdfplumber
 import pandas as pd
 
 def extract_text_from_pdf(pdf_path, section_keyword = "B2"):
-    with `pdfplumber.open(pdf_path) as pdf:
-        tables = page.extract_tables()
-        for table in tables:
-            flat_table = "\n".join([" ".join(row) for row in table])
-            if section_keyword in flat_table:
-                df = pd.DataFrame
-                return df
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            tables = page.extract_tables()
+            for table in tables:
+                flat_table = "\n".join([" ".join(row) for row in table])
+                if section_keyword in flat_table:
+                    df = pd.DataFrame
+                    return df
     return None
